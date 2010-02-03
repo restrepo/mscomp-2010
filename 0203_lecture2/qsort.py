807d520bf89e211c6c4b3e7e401e6b8d2cfa462c
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-02-03 16:01:38 -0500

    Solucion al quicksort

--- 0203_lecture2/qsort.py
+++ 0203_lecture2/qsort.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# coding: utf-8
 """
 From Wikipedia:
 
@@ -13,3 +15,24 @@ function quicksort(array)
 
 Make this a valid Python program and test it.
 """
+
+def quicksort(array):
+    less = []
+    greater = []
+    
+    if len(array) <= 1:
+        return array
+    #select and remove a pivot value pivot from array
+    pivot_idx = 1
+    pivot = array.pop(pivot_idx)
+    
+    for  x in array:
+        if x <= pivot:
+            less.append(x)
+        else:
+            greater.append(x)
+    return quicksort(less) +  [pivot] + quicksort(greater)
+
+
+
+#if __name__ == '__main__':
