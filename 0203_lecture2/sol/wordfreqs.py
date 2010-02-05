dd1110fb80d74e08c3be53bb4f11ea2799c77111
Diego Restrepo <restrepo@gfif.udea.edu.co>
2010-02-05 15:33:48 -0500

    solucion conteo de palabras

---
+++ 0203_lecture2/sol/wordfreqs.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""Word frequencies - count word frequencies in a string.
+
+Open the file HISTORY.gz and construct a histogram of word frequencies.
+Initially, simply consider 'words' to split at whitespace boundaries.
+
+Print the 20 least/most frequent words."""
