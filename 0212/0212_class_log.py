2462e1867ad2a5f4a2b95c1b108acdb8348bfea0
Fernando Perez <Fernando.Perez@berkeley.edu>
2010-02-12 16:31:20 -0500

    Class log and simple recarr example

---
+++ 0212/0212_class_log.py
@@ -0,0 +1,84 @@
+# IPython log file
+
+get_ipython().magic("run error")
+get_ipython().magic("run error")
+
+get_ipython().system("ls -F -F -o --color ")
+get_ipython().system("ls -F -F -o --color ")
+get_ipython().magic("run recarr_simple")
+get_ipython().magic("whos ")
+get_ipython().magic("whos array")
+get_ipython().magic("whos ndarray")
+get_ipython().magic("whos tab")
+get_ipython().magic("whos st")
+get_ipython().magic("whos str")
+get_ipython().magic("whos function")
+get_ipython().system("ls -F -F -o --color ")
+get_ipython().system("clear ")
+get_ipython().system("ls -F -F -o --color ")
+import  strings
+import  string
+string.letters 
+string.ascii_letters 
+string.digits 
+import random
+#?random.Random
+#?random.Random().sample
+rr = random.Random()
+#?rr.sample 
+
+random.Random().sample(string.letters + string.digits , 10)
+''.join(random.Random().sample(string.letters + string.digits , 10))
+''.join(random.Random().sample(string.letters + string.digits , 10))
+''.join(random.Random().sample(string.letters + string.digits , 10))\
+''.join(random.Random().sample(string.letters + string.digits , 10))
+import tempfile
+#?tempfile
+import os
+os.getpid ()
+os.system('ls')
+import commands
+commands.getoutput ('ls')
+print(commands.getoutput ('ls'))
+import subprocess
+get_ipython().system("ls -F ")
+get_ipython().magic("logstart 0212_class_log.py")
+
+a = range(10)
+a
+a[2:4
+]
+a[2:4]
+a[5:]
+a[:5]
+a[:5:2]
+a[:-3]
+a[:-1]
+a[:]
+a[::-1]
+b = 'hola clase de computacion cientifica'
+b
+b[::-1]
+b
+b[0]
+list(b)
+print('-'.join(['hola', 'mundo']))
+print('\n'.join(['hola', 'mundo']))
+print(''.join(['hola', 'mundo']))
+_61
+_60
+''.join(_60)
+''.join(_60)[::-1]
+#?slice
+''.join(_60)[slice(None,None,-1)]
+''.join(_60)[::-1]
+sl = slice(None,None,-1)
+sl
+''.join(_60)[sl]
+alreves = slice(None,None,-1)
+'hola clase'[alreves]
+get_ipython().system("ls -F ")
+get_ipython().magic("run recarr_simple")
+tab
+tab.shape
+get_ipython().system("ls -F -F -o --color ")
